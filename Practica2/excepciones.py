a68c1e513409b667900ad74c19d2e795a5b7d8fd
ARC <rioscarballoa@gmail.com>
2025-05-20 17:56:06 -0600

    Excepciones 1.0

--- Practica2/excepciones.py
+++ Practica2/excepciones.py
@@ -1,5 +1,10 @@
+try:
+    numero = int(input("Introduce un número: "))
+    resultado = 10 / numero
 
-numero = int(input("Introduce un número: "))
-resultado = 10 / numero
-print("El resultado es:", resultado)
+    print("El resultado es:", resultado)
 
+except ZeroDivisionError:
+    print("Error: No se puede dividir entre cero.")
+except ValueError:
+    print("Error: Debes introducir un número entero.")
