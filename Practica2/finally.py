9ef6141aa5d98d10d4d8359ea8cd19b36f0d71ec
ARC <rioscarballoa@gmail.com>
2025-05-21 15:19:24 -0600

    Corrección Finally

--- Practica2/finally.py
+++ Practica2/finally.py
@@ -1,10 +1,10 @@
 try:
     numero = int(input("Escribe un número: "))
-    resultado = 10 / numero
+    resultado = 10 * numero
     print("Resultado:", resultado)
 except ZeroDivisionError:
     print("Error: No puedes dividir entre cero")
 except ValueError:
     print("Error: Eso no es un número")
 finally:
-    print("Vuelva a intentarlo")
+    print("Regrese pronto")
