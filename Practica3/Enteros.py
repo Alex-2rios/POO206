30f48d5fa761c00fafbb6ff902f774ce0e2ed8bd
ARC <rioscarballoa@gmail.com>
2025-05-22 20:05:42 -0600

    No acepta negativo ni 0

--- Practica3/Enteros.py
+++ Practica3/Enteros.py
@@ -2,6 +2,9 @@ while True:
     try:
         
         n = int(input("Escribe un número:"))
+        if n <=0:
+            print("El numero no puede ser cero ni negativo")
+            continue
         if n % 2 == 0:
             print("El número es par")
         else:
