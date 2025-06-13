0ebf99de2e553f9d08f5a0fa730a653d6792646d
ARC <rioscarballoa@gmail.com>
2025-06-12 20:05:07 -0600

    Tipos de rutas

--- FlaskBG/app.py
+++ FlaskBG/app.py
@@ -3,9 +3,31 @@ from flask import Flask
 
 app=Flask(__name__)
 
+#ruta simple
 @app.route('/')
 def home():
     return 'Hola Mundo FLASK'
+
+#ruta con parametros
+@app.route('/saludo/<nombre>')
+def saludar(nombre):
+    return 'Hola, '+nombre+'!!!'
+
+#ruta con try-catch
+@app.errorhandler(404)
+def paginaNoE(e):
+    return 'Cuidado: Error de capa 8 !!!'
+
+#ruta doble
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleroute():
+    return 'Soy el mismo recurso del servidor'
+
+#ruta POST
+@app.route('/formulario' ,methods=['POST'])
+def formulario():
+    return 'Soy un formulario'
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
